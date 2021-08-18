afd5f6b42c3a747689a1d2ed81165283c1daf405
SampabloC <sampablo-jisc@outlook.com>
2021-08-17 23:41:14 -0500

    Add FAQ's page with styles To start creating views that work in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
+  get 'faqs', to: "home#faqs"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
 end
