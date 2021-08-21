ed7ab64130a877e3920f9f00fb85890746887b15
SampabloC <sampablo-jisc@outlook.com>
2021-08-21 00:08:38 -0500

    Add internationalization for English and Spanish Add footer to change language Add faqs view in spanish and english Modify navbar to internationalization Fix Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/:locale' => 'home#index'
   root 'home#index'
-  
-  get 'faqs', to: "home#faqs"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "/:locale" do
+    get 'faqs', to: "home#faqs"
   devise_for :users
+  end
 end
