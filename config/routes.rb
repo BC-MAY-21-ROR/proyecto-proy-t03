14d2ea1f4b51411c635d6f476351716407d80de0
Elizabeth Sarahi Navarro Mateos <esaranm@gmail.com>
2021-08-23 12:52:21 -0500

    Update routes.rb
    Modification of ruotes.rb, route "faqs" that is not used is eliminated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'faqs', to: "home#faqs"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
 end
