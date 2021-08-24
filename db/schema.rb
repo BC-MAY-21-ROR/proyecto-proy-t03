07e71fc24e1da48eaf97b4555ed4ce5ff62094ce
SaraNavarro <esaranm@gmail.com>
2021-08-24 17:48:17 -0500

    Fixing conflicts with _navbar.html.erb and schema.rb files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_211423) do
+ActiveRecord::Schema.define(version: 2021_08_18_053905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 2021_08_11_211423) do
     t.string "gender"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone"
+    t.string "country"
+    t.datetime "birth"
   end
 
   add_foreign_key "appointments", "patients"
