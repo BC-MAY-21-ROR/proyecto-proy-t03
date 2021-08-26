60a836664ff9aa271487bc8991acc243504820e9
xFerchoVJ <fer_elsword@hotmail.com>
2021-08-26 18:58:03 -0500

    Remove migrations
    I've eliminated past migrations that no longer served
    us in the project and caused us errors

--- db/migrate/20210806041300_patients.rb
+++
@@ -1,9 +0,0 @@
-class Patients < ActiveRecord::Migration[6.1]
-  def change
-    create_table :patients do |t|
-      t.belongs_to :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20210818053905_add_fields_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :phone, :string
-    add_column :users, :country, :string
-    add_column :users, :birth, :datetime
-  end
-end
