56ac712d7da7fdd8960fed55ff57606ffeb84a93
martinmt-mx <mmojica0@ucol.mx>
2021-08-30 13:43:35 -0500

    Sing Up

--- db/migrate/20210826212639_delete_tables.rb
+++
@@ -1,8 +0,0 @@
-class DeleteTables < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :appointments
-    drop_table :patients
-    drop_table :psychologists
-    drop_table :users
-  end
-end
