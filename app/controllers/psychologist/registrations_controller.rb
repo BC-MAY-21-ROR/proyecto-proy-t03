ecb40d8b12ba16271d0ab930f03b87bb171c14fa
xFerchoVJ <fer_elsword@hotmail.com>
2021-08-31 17:29:58 -0500

    Fixing bugs and finishing the edit page for Psychologist.
    I've finished giving the styles to the edit page for the psychologist,
    and moved the html
    
    I've moved the html 'cause there was an error with a label that was
    incorrectly placed and didn't allow the user to be updated correctly.

--- app/controllers/psychologist/registrations_controller.rb
+++ app/controllers/psychologist/registrations_controller.rb
@@ -7,9 +7,26 @@ class Psychologist::RegistrationsController < Devise::RegistrationsController
   def new
     super
   end
-    
+   
+  def update
+    super
+  end
+
+  protected
+
+  def after_update_path_for(profile_psychologist)
+    puts 'this is happening yoyo mama'
+    flash[:notice] = "Account succesfully updated"
+     psychologist_path
+  end
+
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :phone, :country, :birth, :description, :professional_register, :speciality, :price])
+  end
+  
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :description, :password, :password_confirmation, :current_password) }
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_214107) do
+ActiveRecord::Schema.define(version: 2021_08_31_213231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_08_26_214107) do
     t.string "phone"
     t.string "country"
     t.datetime "birth"
-    t.string "description"
+    t.text "description"
     t.string "professional_register"
     t.string "speciality"
     t.money "price", scale: 2
