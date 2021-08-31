768c4ec09cf8a44dd6a7725ad288abc4bf86aba6
xFerchoVJ <fer_elsword@hotmail.com>
2021-08-31 17:29:58 -0500

    Fixing bugs and finishing the edit page for Psychologist.
    I've finished giving the styles to the edit page for the psychologist,
    and moved the html
    
    I've moved the html 'cause there was an error with a label that was
    incorrectly placed and didn't allow the user to be updated correctly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :phone, :country, :birth, :description, :professional_register, :speciality, :price])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :gender, :phone, :country, :birth, :description, :professional_register, :speciality, :price])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :description, :password, :password_confirmation, :current_password) }
   end
 end
